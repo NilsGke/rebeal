1ec793ecddd504e048d139d62223b583393ff865
NilsGke <n.j.goeke@gmail.com>
2023-06-21 21:13:14 +0200

    pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({});
+
+module.exports = nextConfig;
--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,3 +1,39 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  applicationName: "BeReal.",
+  appleWebApp: true,
+  description: 'a clone of the app "BeReal."',
+  formatDetection: {
+    telephone: true,
+    date: true,
+    address: true,
+    email: true,
+    url: true,
+  },
+  themeColor: "#000000",
+  icons: [
+    {
+      url: "/logo/icon-192x192.png",
+      sizes: "192x192",
+    },
+    {
+      url: "/logo/icon-256x256.png",
+      sizes: "256x256",
+    },
+    {
+      url: "/logo/icon-384x384.png",
+      sizes: "384x384",
+    },
+    {
+      url: "/logo/icon-512x512.png",
+      sizes: "512x512",
+    },
+  ],
+  manifest: "/manifest.json",
+  colorScheme: "dark",
+};
+
 export default function AppContainer({
   children,
 }: {
