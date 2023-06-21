05beb52591298b2dc5c80a83c5e430d49f509a0f
NilsGke <n.j.goeke@gmail.com>
2023-06-21 19:44:34 +0200

    admin firebase app

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
-import { getStorage } from "firebase/storage";
 import { getApps, initializeApp } from "firebase/app";
+
+import admin from "firebase-admin";
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,4 +14,26 @@ export const firebaseConfig = {
 export const firebase_app =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-export const storage = getStorage(firebase_app);
+const adminConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
+};
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+  });
+} catch (error) {
+  /*
+   * We skip the "already exists" message which is
+   * not an actual error when we're hot-reloading.
+   */
+  if (!/already exists/u.test((error as any).message)) {
+    console.error("Firebase admin initialization error", (error as any).stack);
+  }
+}
+
+export default admin;
