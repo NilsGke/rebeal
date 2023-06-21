a8fae081c4d1f202b7b433cf4b79b804bfa2576a
NilsGke <n.j.goeke@gmail.com>
2023-06-21 19:44:22 +0200

    shutter button

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -7,19 +7,17 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 export default function App() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   if (!session || session.user === undefined)
     return <p>you need to log in to use this app</p>;
 
   return (
     <>
-      <header className="sticky w-full max-w-3xl top-2 p-3 flex flex-row justify-between items-center">
-        <Link href="/app/friends">
+      <header className="sticky w-full max-w-3xl top-2 p-3 flex flex-row justify-center items-center">
+        <Link className="absolute left-3" href="/app/friends">
           <PeopleIcon />
         </Link>
         <h1 className="text-2xl">ReBeal.</h1>
-        <Link href="/app/profile">
+        <Link className="absolute right-3" href="/app/profile">
           {typeof session.user.image === "string" ? (
             // eslint-disable-next-line @next/next/no-img-element
             <img
@@ -33,6 +31,10 @@ export default function App() {
         </Link>
       </header>
       <main></main>
+      <Link
+        className="absolute bottom-5 left-[calc(50%-(5rem/2))] rounded-full h-20 w-20 border-4 active:bg-white"
+        href="app/upload"
+      ></Link>
     </>
   );
 }
