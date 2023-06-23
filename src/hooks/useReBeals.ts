39dcdaea7923eda0cba3d1acf4b86d67c0a10e27
NilsGke <n.j.goeke@gmail.com>
2023-06-23 23:48:26 +0200

    handle bereals (realtime updates)

---
+++ src/hooks/useReBeals.ts
@@ -0,0 +1,106 @@
+"use client";
+
+import firebase_app from "@/firebase/client";
+import {
+  collection,
+  doc,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+  DocumentData,
+  QueryDocumentSnapshot,
+  getDocs,
+  DocumentReference,
+} from "firebase/firestore";
+import { User } from "next-auth";
+import { useEffect, useRef, useState } from "react";
+
+type ReBeal = {
+  id: string;
+  user: DocumentReference;
+  images: {
+    environment: string;
+    selfie: string;
+  };
+  postedAt: number;
+  late?: boolean;
+};
+
+// const firestore = initializeApp(firebaseConfig);
+const firestore = getFirestore(firebase_app);
+
+const useReBeals = (userId: string, friendIds: string[]) => {
+  const [reBeals, setRebeals] = useState<ReBeal[]>([]);
+  const [newReBeals, setNewReBeals] = useState<ReBeal[]>([]);
+  const newAvalible = newReBeals.length > 0;
+
+  console.log({ rebeals: reBeals, newReBeals, newAvalible });
+
+  const showNewReBeals = () => {
+    setRebeals((prev) => [...prev, ...newReBeals]);
+    setNewReBeals([]);
+  };
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    const rebealQuery = query(
+      collection(firestore, "rebeals"),
+      where(
+        "user",
+        "in",
+        friendIds.map((id) => doc(firestore, "users", id))
+      )
+    );
+
+    const docToReBeal = (doc: QueryDocumentSnapshot<DocumentData>): ReBeal => ({
+      ...(doc.data() as Omit<ReBeal, "id">),
+      id: doc.id,
+    });
+
+    const unsub = onSnapshot(rebealQuery, (snapshot) => {
+      setRebeals((prevReBeals) => {
+        let reBeals = prevReBeals.slice();
+        setNewReBeals((prevNewReBeals) => {
+          const newReBeals = prevNewReBeals.slice();
+          snapshot.docChanges().forEach((change) => {
+            if (change.type === "added")
+              if (firstRender.current) {
+                reBeals.push(docToReBeal(change.doc));
+                firstRender.current = false;
+              } else newReBeals.push(docToReBeal(change.doc));
+
+            if (change.type === "modified")
+              reBeals = reBeals
+                .slice()
+                .map((r) =>
+                  r.id !== change.doc.id ? r : (change.doc.data() as ReBeal)
+                );
+
+            if (change.type === "removed")
+              reBeals.splice(
+                reBeals.findIndex((r) => r.id === change.doc.id),
+                1
+              );
+          });
+
+          return newReBeals;
+        });
+
+        return reBeals;
+      });
+    });
+    console.log("listening");
+
+    return () => {
+      console.log("not listening");
+
+      unsub();
+    };
+  }, [friendIds]);
+
+  return { reBeals, newAvalible, showNewReBeals };
+};
+
+export default useReBeals;
