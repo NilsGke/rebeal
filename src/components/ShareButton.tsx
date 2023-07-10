cdee5ff7c55867f8bff09c4b20d1307477b6af0b
NilsGke <n.j.goeke@gmail.com>
2023-07-10 17:09:19 +0200

    new props for ShareButton (enable more usecases)

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -2,8 +2,20 @@
 
 import Image from "next/image";
 import ShareIcon from "../../public/assets/share.svg";
+import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-const ShareButton = ({ url }: { url: string }) => {
+const ShareButton = ({
+  url,
+  children,
+  className = "",
+  whiteIcon = false,
+}: {
+  url: string;
+  children?: ReactNode;
+  className?: string;
+  whiteIcon?: boolean;
+}) => {
   const share = () =>
     navigator.share({
       url,
@@ -11,9 +23,17 @@ const ShareButton = ({ url }: { url: string }) => {
   return (
     <button
       onClick={share}
-      className="h-8 aspect-square flex justify-center items-center rounded-full bg-gray-50"
+      className={twMerge(
+        "h-8 aspect-square flex justify-center items-center rounded-full bg-gray-50",
+        className
+      )}
     >
-      <Image className="h-5 w-5" src={ShareIcon} alt="share" />
+      <Image
+        className={"h-5 w-5" + (whiteIcon ? " invert" : "")}
+        src={ShareIcon}
+        alt="share"
+      />{" "}
+      {children}
     </button>
   );
 };
