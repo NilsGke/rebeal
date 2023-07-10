d2c1cea5e28e89cb2e47ecbc1e5f82bbf8283946
NilsGke <n.j.goeke@gmail.com>
2023-07-10 17:08:29 +0200

    new converters

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,21 +1,25 @@
-import admin from "@/firebase/config";
+import { PartialBy } from "@/helpers/tshelpers";
 import {
   DocumentReference as AdminDocumentReference,
   Timestamp as AdminTimestamp,
+  QueryDocumentSnapshot as AdminQueryDocumentSnapshot,
+  FirestoreDataConverter,
 } from "firebase-admin/firestore";
 import {
   DocumentReference as ClientDocumentReference,
   Timestamp as ClientTimestamp,
+  QueryDocumentSnapshot,
 } from "firebase/firestore";
+import { User } from "next-auth";
 
 export type ReBeal = {
   id: string;
-  user: AdminDocumentReference | ClientDocumentReference;
+  user: AdminDocumentReference;
   images: {
     environment: string;
     selfie: string;
   };
-  postedAt: AdminTimestamp | ClientTimestamp;
+  postedAt: AdminTimestamp;
   late?: boolean;
 };
 
@@ -23,4 +27,39 @@ export const rebealConverter = {
   toFirestore: (data: ReBeal) => data,
   fromFirestore: (snap: FirebaseFirestore.QueryDocumentSnapshot) =>
     ({ ...snap.data(), id: snap.id } as ReBeal),
+};
+
+export const userConverter: FirestoreDataConverter<User> = {
+  toFirestore: (data: User) => {
+    const user: Omit<User, "id"> = {
+      email: data.email,
+      image: data.image,
+      name: data.name,
+    };
+    return user;
+  },
+  fromFirestore: (snap: AdminQueryDocumentSnapshot) =>
+    ({
+      ...snap.data(),
+      id: snap.id,
+    } as User),
+};
+
+export type Comment = {
+  content: string;
+  user: AdminDocumentReference;
+  postedAt: AdminTimestamp;
+  id: string;
+};
+
+export const commentConverter = {
+  toFirestore: (data: PartialBy<Comment, "id">) => {
+    delete data.id;
+    ({ ...data, id: undefined });
+  },
+  fromFirestore: (snap: AdminQueryDocumentSnapshot) =>
+    ({
+      ...snap.data(),
+      id: snap.id,
+    } as Comment),
 };
