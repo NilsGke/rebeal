2189ccc179220ea127a6601186ed999b6c14d168
NilsGke <n.j.goeke@gmail.com>
2023-08-22 03:02:42 +0200

    add `fromTimestamp` parameter

--- src/firebase/server/getReBeals.ts
+++ src/firebase/server/getReBeals.ts
@@ -1,11 +1,23 @@
 import { rebealConverter } from "@/app/types";
 import admin from "../config";
 import getUsers from "./getUsers";
+import { Timestamp } from "firebase-admin/firestore";
 
 const firestore = admin.firestore();
 
-export default async function getReBeals(userIds: string[]) {
+export default async function getReBeals(
+  userIds: string[],
+  fromTimestamp?: Timestamp | Date
+) {
   if (userIds.length === 0) return [];
+
+  // convert Date to Timestamp
+  fromTimestamp =
+    fromTimestamp instanceof Date
+      ? Timestamp.fromDate(fromTimestamp)
+      : fromTimestamp;
+
+  fromTimestamp = fromTimestamp || Timestamp.fromDate(new Date(0));
 
   const userDocs = userIds.map((id) => firestore.doc(`users/${id}`));
 
@@ -13,6 +25,7 @@ export default async function getReBeals(userIds: string[]) {
     await firestore
       .collection("rebeals")
       .where("user", "in", userDocs)
+      .where("postedAt", ">=", fromTimestamp)
       .withConverter(rebealConverter)
       .get()
   ).docs.map((d) => {
