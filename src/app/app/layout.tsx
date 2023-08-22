84cebb3b0aaf6006e7a9c1d81eaab4464eef9095
NilsGke <n.j.goeke@gmail.com>
2023-08-22 03:12:01 +0200

    position relative to allow for absolute positioning in children

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -40,7 +40,7 @@ export default function AppContainer({
   children: React.ReactNode;
 }) {
   return (
-    <div className="aspect-[9/16] w-screen min-[500px]:h-screen min-[500px]:w-auto">
+    <div className="aspect-[9/16] w-screen min-[500px]:h-screen min-[500px]:w-auto relative">
       {children}
     </div>
   );
