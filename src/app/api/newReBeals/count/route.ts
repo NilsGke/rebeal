ec909bc85ac0a79fa7abc641d5e1d758813b0b9c
NilsGke <n.j.goeke@gmail.com>
2023-08-22 03:03:02 +0200

    count from lastTTRB

--- src/app/api/newReBeals/count/route.ts
+++ src/app/api/newReBeals/count/route.ts
@@ -2,12 +2,16 @@ import { NextResponse } from "next/server";
 import serverAuth from "@/helpers/serverComponentAuth";
 import getFriends from "@/firebase/server/getFriends";
 import admin from "@/firebase/config";
+import getLastTTRB from "@/firebase/server/getLastTTRB";
+import { Timestamp } from "firebase-admin/firestore";
 
 export async function GET() {
   const session = await serverAuth();
   const friendIds = await getFriends(session.user.id);
 
   if (friendIds.length === 0) return NextResponse.json({ count: 0 });
+
+  const lastTTRB = await getLastTTRB();
 
   const firestore = admin.firestore();
 
@@ -18,6 +22,7 @@ export async function GET() {
       "in",
       friendIds.map((id) => firestore.doc(`users/${id}`))
     )
+    .where("postedAt", ">=", Timestamp.fromDate(lastTTRB.announcedAt))
     .count()
     .get();
 
