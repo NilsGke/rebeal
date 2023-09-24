0a9978250e72a9371ea8d17ef52c1f16ae013417
NilsGke <n.j.goeke@gmail.com>
2023-09-24 21:03:12 +0200

    small fix

--- src/helpers/isStandalone.ts
+++ src/helpers/isStandalone.ts
@@ -1,9 +1,12 @@
+"use client";
+
 /**
  * returns boolean indicating if running in installed PWA
  */
 export default function isStandalone() {
   const mqStandAlone = "(display-mode: standalone)";
   return (
-    (navigator as any).standalone || window.matchMedia(mqStandAlone).matches
+    (global.navigator as any).standalone ||
+    window.matchMedia(mqStandAlone).matches
   );
 }
